b778af9aaeb3a47a384f8a93008ba75327f48b56
aurobindo-gupta <aurobindo@wednesday.is>
2021-09-14 19:03:18 +0530

    data saved in redux

--- app/containers/Itunes/saga.js
+++ app/containers/Itunes/saga.js
@@ -2,16 +2,16 @@ import { itunesApi } from '@app/services/itunesApi';
 import { takeLatest, call, put } from 'redux-saga/effects';
 import { itunesTypes, itunesCreators } from './reducer';
 // Individual exports for testing
-const { REQUEST_GET_ITUNE_LIST } = itunesTypes;
+const { REQUEST_GET_ITUNES_LIST } = itunesTypes;
 const { successGetItunesList, failureGetitunesList } = itunesCreators;
 
 export function* getItuneData(action) {
   console.log('saga==', action.ituneName);
   const response = yield call(itunesApi, action.ituneName);
   const { data } = response;
-  if (ok) {
+  if (data) {
     yield put(successGetItunesList(data));
-    console.log('insaga');
+    console.log('insaga', data);
   } else {
     yield put(failureGetitunesList(data));
     console.log('insaga fail');
@@ -19,5 +19,5 @@ export function* getItuneData(action) {
 }
 
 export default function* itunesSaga() {
-  yield takeLatest(REQUEST_GET_ITUNE_LIST, getItuneData);
+  yield takeLatest(REQUEST_GET_ITUNES_LIST, getItuneData);
 }
--- app/services/itunesApi.js
+++ app/services/itunesApi.js
@@ -1,9 +1,9 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export const itunesApi=(txt)=>{
+export const itunesApi = (txt) => {
   console.log(txt);
   return axios.request({
-    method: "get",
-    url:`https://itunes.apple.com/search?term=${txt}`
+    method: 'get',
+    url: `https://itunes.apple.com/search?term=${txt}`
   });
-}
+};
