738d46e9ce0830c25bb7694ed0785bb4c398391c
aurobindo-gupta <aurobindo@wednesday.is>
2021-09-27 12:38:12 +0530

    fix: test api filing

--- app/services/tests/repoApi.test.js
+++ app/services/tests/repoApi.test.js
@@ -1,31 +1,19 @@
-import { itunesApi } from '../itunesApi';
-import { trackIdApi } from '../trackIdApi';
-import * as axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { getApiClient } from '@utils/apiUtils';
+import { getRepos } from '../repoApi';
 
-jest.mock('axios');
-describe('Api tests', () => {
-  const trackId = '120954025';
-  // const repositoryName = 'mac';
-  // it('should make the api call to "/search/repositories?q="', async () => {
-  //   const mock = new MockAdapter(getApiClient().axiosInstance);
-  //   const data = [
-  //     {
-  //       totalCount: 1,
-  //       items: [{ repositoryName }]
-  //     }
-  //   ];
-  //   mock.onGet(`/search/repositories?q=${repositoryName}`).reply(200, data);
-  //   const res = await getRepos(repositoryName);
-  //   expect(res.data).toEqual(data);
-  // });
-  it('should fetch Itunes data', async () => {
-    const response = { data: { results: [{ artistName: 'adele' }] } };
-    axios.request.mockResolvedValue(response);
-    expect(await itunesApi('adele')).toEqual(response);
-  });
-  it('should fetch track data', async () => {
-    const response = { data: { results: [{ artistName: 'Jack Johnson' }] } };
-    axios.request.mockResolvedValue(response);
-    expect(await trackIdApi(trackId)).toEqual(response);
+describe('RepoApi tests', () => {
+  const repositoryName = 'mac';
+  it('should make the api call to "/search/repositories?q="', async () => {
+    const mock = new MockAdapter(getApiClient().axiosInstance);
+    const data = [
+      {
+        totalCount: 1,
+        items: [{ repositoryName }]
+      }
+    ];
+    mock.onGet(`/search/repositories?q=${repositoryName}`).reply(200, data);
+    const res = await getRepos(repositoryName);
+    expect(res.data).toEqual(data);
   });
 });
